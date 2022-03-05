473a8e1f5cfbd6c902e993428e5621c96b0839bd
sueleesoossoo <isoo7510@gmail.com>
2022-03-05 21:21:54 +0900

    init mui theme, add storybook addon mui

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
+import { muiTheme } from 'storybook-addon-material-ui';
+import theme from '../src/styles/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [muiTheme([theme])];
